1a01c7f238a3df3d95d0e5ef9fac44eab07ff835
nashva <nashvakizhakkayil@gmail.com>
2023-10-02 22:40:06 +0530

    exploring dialog box

---
+++ lib/route_management/dialog.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class Dialogbox extends StatelessWidget {
+  const Dialogbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: () {
+          Get.defaultDialog(
+              title: 'Delete record',
+              middleText: 'Doyou want to delete this record',
+              backgroundColor: Colors.amberAccent,
+              radius: 40,
+              //if we use both content and middle text ,the content overrides the middletext
+              // content: Column(
+              //   children: [
+              //     Text('Data loading '),
+              //     Text('dsf'),
+              //   ],
+              // ),
+              contentPadding: EdgeInsets.all(10),
+              titlePadding: EdgeInsets.all(20),
+              // textCancel: 'Cancel',  //default style
+              // textConfirm: 'Ok',
+              // onCancel: () {},
+              // onConfirm: () {},
+              //buttonColor: Colors.green,
+              //to create customized button we can use confirm instead of textConfirm
+              confirm: TextButton(
+                onPressed: () {
+                  //Navigator.pop(context);
+                  Get.back();
+                },
+                child: Text("Ok"),
+              ),
+              //////////while clicking outside the dialog box ,it doesnot pop
+              barrierDismissible: false);
+        },
+        child: Text('Show dialog'));
+  }
+}
--- lib/route_management/snackbar.dart
+++ lib/route_management/snackbar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_sample/route_management/dialog.dart';
 
 class SnackbarEx extends StatelessWidget {
   const SnackbarEx({super.key});
@@ -9,28 +10,36 @@ class SnackbarEx extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('getx snackbar')),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Get.snackbar(
-              'Showing snackbar',
-              'Todo added',
-              backgroundColor: Colors.red.shade200,
-              messageText: Text('Overriding the above message'),
-              titleText: Text('Overriding the above first title'),
-              isDismissible: true,
-              dismissDirection: DismissDirection.horizontal,
-              forwardAnimationCurve: Curves.bounceInOut,
-              duration: Duration(seconds: 3),
-              icon: Icon(Icons.send),
-              mainButton: TextButton(
-                onPressed: () {},
-                child: Text('Retry'),
-              ),
-              onTap: (val) {}, //while clicking the snackbar
-              overlayBlur: 6,
-            );
-          },
-          child: const Text('Show snackbar'),
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Get.snackbar(
+                  'Showing snackbar',
+                  'Todo added',
+                  backgroundColor: Colors.red.shade200,
+                  messageText: Text('Overriding the above message'),
+                  titleText: Text('Overriding the above first title'),
+                  isDismissible: true,
+                  dismissDirection: DismissDirection.horizontal,
+                  forwardAnimationCurve: Curves.easeIn,
+                  duration: Duration(seconds: 3),
+                  icon: Icon(Icons.send),
+                  mainButton: TextButton(
+                    onPressed: () {},
+                    child: Text('Retry'),
+                  ),
+                  onTap: (val) {}, //while clicking the snackbar
+                  overlayBlur: 6, //blur the screen
+                );
+              },
+              child: const Text('Show snackbar'),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Dialogbox(),
+          ],
         ),
       ),
     );
