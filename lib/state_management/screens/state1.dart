957986f5d408df7e771cd02241f9f1884dafc1c5
nashva <nashvakizhakkayil@gmail.com>
2023-10-13 12:06:05 +0530

    state management using getx() is created

--- lib/state_management/screens/state1.dart
+++ lib/state_management/screens/state1.dart
@@ -4,27 +4,73 @@ import 'package:getx_sample/state_management/screens/second2.dart';
 
 import '../controller/counter_controller.dart';
 
+// class StateOne extends StatelessWidget {
+//   StateOne({super.key});
+//   final CounterController controller = Get.put(CounterController());
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('first screen'),
+//       ),
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         crossAxisAlignment: CrossAxisAlignment.center,
+//         children: [
+//           Obx(
+//             () => Center(
+//               child: Text(
+//                 controller.counter.toString(),
+//                 style: TextStyle(fontSize: 30),
+//               ),
+//             ),
+//           ),
+//           SizedBox(
+//             height: 30,
+//           ),
+//           TextButton(
+//               onPressed: () {
+//                 Get.to(
+//                   StateSecond(),
+//                 );
+//               },
+//               child: Text('next')),
+//         ],
+//       ),
+//       floatingActionButton: FloatingActionButton(onPressed: () {
+//         controller.incrementCounter();
+//       }),
+//     );
+//   }
+// }
+
+//
+
+//      U S I N G   G E T X
+
 class StateOne extends StatelessWidget {
   StateOne({super.key});
-  final CounterController controller = Get.put(CounterController());
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('first screen'),
+        title: Text('Counter screen'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Obx(
-            () => Center(
-              child: Text(
-                controller.counter.toString(),
-                style: TextStyle(fontSize: 30),
-              ),
-            ),
-          ),
+          GetX<CounterController>(
+              init: CounterController(),
+              builder: (controller) {
+                return Center(
+                  child: Text(
+                    controller.counter.toString(),
+                    style: TextStyle(fontSize: 30),
+                  ),
+                );
+              }),
           SizedBox(
             height: 30,
           ),
@@ -38,8 +84,12 @@ class StateOne extends StatelessWidget {
         ],
       ),
       floatingActionButton: FloatingActionButton(onPressed: () {
-        controller.incrementCounter();
+        Get.find<CounterController>().incrementCounter();
       }),
     );
   }
 }
+
+
+
+//   U S I N G   G E T B U I L D E R
